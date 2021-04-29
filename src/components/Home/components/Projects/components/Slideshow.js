0c4071d370a4ea1cca77b421d63e2e374ce38c25
Anirudh Giran <anirudhgiran56@gmail.com>
2021-04-29 15:30:06 -0400

    working on home/slideshow

--- src/components/Home/components/Projects/components/Slideshow.js
+++ src/components/Home/components/Projects/components/Slideshow.js
@@ -3,7 +3,30 @@ import React from "react";
 import "./Slideshow.scss";
 
 const Slideshow = () => {
-    return <div className="slideshow-container"></div>;
+    return (
+        <div className="slideshow-container">
+            <div className="slides">
+                <div className="slide">
+                    <img
+                        src={require("../../../../../assets/images/leica.PNG")}
+                        alt=""
+                    />
+                </div>
+                <div className="slide">
+                    <img
+                        src={require("../../../../../assets/images/cart.PNG")}
+                        alt=""
+                    />
+                </div>
+                <div className="slide">
+                    <img
+                        src={require("../../../../../assets/images/warisvirsede.PNG")}
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Slideshow;
